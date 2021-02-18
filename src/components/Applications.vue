d06386139b6098965d0290207432d6ed59471a59
Bobby Walters <30978105+ojabi@users.noreply.github.com>
2021-02-18 07:27:09 -0800

    Using Vue Template grouping

--- src/components/Applications.vue
+++ src/components/Applications.vue
@@ -1,13 +1,27 @@
 <template>
-    <div class="should-have-a-children scroll-me overflow:auto">
+  <template v-if="show-brief">
       <div class="applications">
-        <input type="date" name="date" />
-        <input type="text" name="position" placeholder="Position" />
-        <input type="text" name="company" placeholder="Company" />
+        <input type="date" name="date" key="date"/>
+        <input type="text" name="position" placeholder="Position" key="position" />
+        <input type="text" name="company" placeholder="Company" key="company" />
         <button name="view_details" v-on:click.left="ViewDetails({})">View Details</button>
-        <router-link to="/application-details" tag="button">Application Details</router-link>
       </div>
-    </div>
+  </template>
+  <template v-else-if="show-details">
+    <br /><input type="text" name="location" placeholder="City, ST" />
+    <input type="text" name="contact_first_name" placeholder="John" />
+    <input type="text" name="contact_last_name" placeholder="Doe" /><br />
+    <input type="text" name="contact_phone" placeholder="xxx-xxx-xxxx" />
+    <select name="application_status">
+      <option name="applied" value="Applied" />
+      <option name="notapplied" value="Not Applied" />
+    </select>
+     <select name="application_method">
+      <option name="resume" value="Resume" />
+      <option name="application" value="Application" /><br />
+      
+    </select>
+  </template>
 </template>
 
 <script>
