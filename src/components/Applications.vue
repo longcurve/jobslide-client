2419bc4d933c4f231e68319fd9e9345ca6997dbd
Bobby Walters <30978105+ojabi@users.noreply.github.com>
2021-06-25 06:13:09 -0700

    Application form not showing up

--- src/components/Applications.vue
+++ src/components/Applications.vue
@@ -2,25 +2,41 @@
   <form name="applications" method="GET">
   <template v-if="true">
       <div class="applications">
-        <input type="date" name="date" key="date"/>
-        <input type="text" name="position" placeholder="Position" key="position" />
-        <input type="text" name="company" placeholder="Company" key="company" />
+        <label for="date">Date:</label>
+        <input type="date" name="date" key="date"/><br />
+        
+        <label for="position">Position</label>
+        <input type="text" name="position" placeholder="Position" key="position" /><br />
+        
+        <label for="company">Company</label>
+        <input type="text" name="company" placeholder="Company" key="company" /><br />
+        
         <button type="button" name="view_details" v-on:click.left="viewDetails({})">{{ showdetails ? 'Hide Details' : 'View Details' }}</button>
       </div>
   </template>
   <template v-if="showdetails">
-    <br /><input type="text" name="location" placeholder="City, ST" />
-    <input type="text" name="contact_first_name" placeholder="John" />
-    <input type="text" name="contact_last_name" placeholder="Doe" /><br />
-    <input type="text" name="contact_phone" placeholder="xxx-xxx-xxxx" />
+    <br />
+    <label for="CityState">City, State</label>
+    <input type="text" name="location" placeholder="City, ST" /><br />
+    
+    <label for="ContactName">Contact Name</label>
+    <input type="text" name="contact_first_name" placeholder="John" /><br />
+
+    <label for="ContactPhone">Contact Phone</label>
+    <input type="text" name="contact_phone" placeholder="xxx-xxx-xxxx" /><br />
+
+    <label for="ApplicationStatus">Application Status</label>
     <select name="application_status">
       <option name="applied" value="Applied" />
       <option name="notapplied" value="Not Applied" />
-    </select>
+    </select><br />
+
+    <label for="ApplicationMethod">Application Method</label>
      <select name="application_method">
       <option name="resume" value="Resume" />
       <option name="application" value="Application" />
-    </select>
+    </select><br />
+
      <button
       type="button"
       @click="showInterviewsModal = !showInterviewsModal"
@@ -51,6 +67,7 @@ export default {
     },
     loadApplications() {
       // Load applications from server
+      return ["01/01/21", "Position", "Company", "CityState"];
     },
     viewDetails() {
       // Expand application view to show all details OR collapse application view
@@ -63,13 +80,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
-
-select {
-  margin-left: 10px;
-}
-</style>
